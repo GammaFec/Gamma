24131bd61e2bcfdf6167e9b8b849b73613602611
Agustin Vazquez <agustinvazquez453@gmail.com>
2020-12-23 15:33:29 -0300

    Agrego estructura de rutas

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './routes/AppRouter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Layout from '../components/Layout';
+import PrivateRoute from './PrivateRoute';
+
+const Login = React.lazy(() => import('../pages/Login'));
+
+const AppRouter = () => {
+  return (
+    <>
+      <Suspense fallback="Cargando pa">
+        <Router>
+          <Switch>
+            <Route path="/login" component={Login} />
+
+            <PrivateRoute path="/">
+              <Layout />
+            </PrivateRoute>
+          </Switch>
+        </Router>
+
+      </Suspense>
+    </>
+  );
+};
+
+export default AppRouter;

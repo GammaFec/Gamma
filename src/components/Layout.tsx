4e729c7f6133cb0bfcb13f13d1f75d10f1c22936
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-02 14:59:50 -0300

    Agrego eslint y prettier

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,31 +3,27 @@ import { Switch, Route } from 'react-router-dom';
 import Routes from '../routes/routes';
 
 const Layout = () => {
+    const getRoutes = (routes: any): ReactNode => {
+        return routes.map((route: any) => {
+            return (
+                <Route
+                    path={route.path}
+                    component={route.component}
+                    exact={route.exact && route.exact}
+                    key={route.name}
+                />
+            );
+        });
+    };
 
-  const getRoutes = (routes: any): ReactNode => {
-    return routes.map((route: any) => {
-      return (
-        <Route
-          path={route.path}
-          component={route.component}
-          exact={route.exact && route.exact}
-          key={route.name}
-        />
-      );
-    });
-  };
-
-
-  return (
-    <>
-      <Suspense fallback="cargando pa">
-        Common components
-        <Switch>
-          {getRoutes(Routes)}
-        </Switch>
-      </Suspense>
-    </>
-  );
+    return (
+        <>
+            <Suspense fallback="cargando pa">
+                Common components
+                <Switch>{getRoutes(Routes)}</Switch>
+            </Suspense>
+        </>
+    );
 };
 
 export default Layout;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const NotFound = () => {
-  return (
-    <div>
-      NotFound
-    </div>
-  );
+    return <div>NotFound</div>;
 };
 
 export default NotFound;

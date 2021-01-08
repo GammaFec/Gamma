f9e4ea4f7141d690bd4014bdced1788c74155589
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-08 10:32:44 -0300

    [fix] uso clase de la configuracion de firebase

--- src/services/firebase/auth/sendSMSCode.ts
+++ src/services/firebase/auth/sendSMSCode.ts
@@ -1,5 +1,7 @@
 import firebase from 'firebase';
+import FirebaseApp from '../config';
 
-const sendSMSCode = async (phoneNumber: string, appVerifier: firebase.auth.RecaptchaVerifier): Promise<firebase.auth.ConfirmationResult> => firebase.auth().signInWithPhoneNumber(`+${phoneNumber}`, appVerifier);
+const sendSMSCode = async (phoneNumber: string, appVerifier: firebase.auth.RecaptchaVerifier): Promise<firebase.auth.ConfirmationResult> => FirebaseApp.getInstance().auth().signInWithPhoneNumber(`+${phoneNumber}`, appVerifier);
+
 
 export default sendSMSCode;

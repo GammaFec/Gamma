0344c726ff3e4390f4fa147074583549588bb915
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-11 19:48:35 -0300

    Se agrego el tipo al return

--- src/components/Carousel/utils.tsx
+++ src/components/Carousel/utils.tsx
@@ -9,7 +9,7 @@ const carouselData = Jsondata.CarouselData;
 /**Map the Data used in the carousel to build as many Tabs as nedded.
  * It is included the Img and P Tabs and they are fill with the data that we fetch from the Data (Json).
  */
-const carouselDataMapped = (activeTabIndex: number) =>
+const carouselDataMapped = (activeTabIndex: number): React.ReactNode =>
     carouselData.map((dataObj, index) => {
         return (
             <StyledLi key={dataObj.id} active={activeTabIndex === index}>

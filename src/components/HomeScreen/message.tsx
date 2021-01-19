ae90c0e9fc42486a8cea0b5f962fa37ff81e5d68
Nacho <nachoherrero93@hotmail.com>
2021-01-19 16:34:49 -0300

    cambio el nombre del directorio del componente

---
+++ src/components/HomeScreen/message.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from "react";
+
+// Components
+import Paragraph from "./paragraph";
+import Title from "./title";
+
+const Mess = (): ReactElement => {
+    return (
+        <>
+            <Title title={"hola"} />
+            <Paragraph text={"parrafo"} />
+        </>
+    );
+};
+
+export default Mess;

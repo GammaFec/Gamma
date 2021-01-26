1820244a95f3f5ddda1906101d4f61f7dbd84d83
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-01-26 20:24:19 -0300

    cambio nombre de la interfaz en archivo de estilos

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from "styled-components";
-import { FormProps } from "./types";
+import iForm from "./types";
 
 export const StyledForm = styled.form`
-    ${({ isContentCentered }: FormProps) =>
+    ${({ isContentCentered }: iForm) =>
         isContentCentered &&
         css`
             text-align: center;

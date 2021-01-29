1ebeaaf527c211fd6bb6cfe26c19358d9157ad0d
Nacho <nachoherrero93@hotmail.com>
2021-01-29 15:31:52 -0300

    paragraph.tsx title.tsx: lleve el tipado al mismo componente

--- src/components/HomeScreen/paragraph.tsx
+++ src/components/HomeScreen/paragraph.tsx
@@ -1,7 +1,5 @@
 import React, { ReactElement } from "react";
 
-// Others
-import { IParagraph } from "./types";
+const Paragraph = ({ text }: { text: string }): ReactElement => <p>{text}</p>;
 
-const Paragraph = ({ text }: IParagraph): ReactElement => <p>{text}</p>;
 export default Paragraph;
--- src/components/HomeScreen/title.tsx
+++ src/components/HomeScreen/title.tsx
@@ -1,7 +1,5 @@
 import React, { ReactElement } from "react";
 
-// Others
-import { ITitle } from "./types";
+const Title = ({ title }: { title: string }): ReactElement => <h1>{title}</h1>;
 
-const Title = ({ title }: ITitle): ReactElement => <h1>{title}</h1>;
 export default Title;

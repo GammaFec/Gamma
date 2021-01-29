2771f38ea316526d10f64de9a8b98464d7988752
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-28 22:04:35 -0300

    Solucion a conflicts

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -5,17 +5,17 @@ export const SocialMedia = () => {
         <div className="social">
             <ul className="social__list">
                 <li className=".social__facebook">
-                    <a href="#">
+                    <a href="https://www.google.com/">
                         <i className="bx bxl-facebook-circle"></i>
                     </a>
                 </li>
                 <li className=".social__instagram">
-                    <a href="">
+                    <a href="https://www.google.com/">
                         <i className="bx bxl-instagram-alt"></i>
                     </a>
                 </li>
                 <li className=".social__gmail">
-                    <a href="">
+                    <a href="https://www.google.com/">
                         <i className="bx bxl-google"></i>
                     </a>
                 </li>

3e4c4733b11d486d313cf774f281e4d1240b77bf
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-02-01 12:28:01 -0300

    returns implicitos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Home = (): JSX.Element => {
-    return <div>Home</div>;
-};
+const Home = (): JSX.Element => <div>Home</div>;
 
 export default Home;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Login = (): JSX.Element => {
-    return <div>Login</div>;
-};
+const Login = (): JSX.Element => <div>Login</div>;
 
 export default Login;

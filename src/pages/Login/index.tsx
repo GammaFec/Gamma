b671a79a583438137a7be599be27b9e0fe680970
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-04 18:06:50 -0300

    Se agrego los estados para los value de los Input InputHandlers creado en su versiona basica

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import { strict } from "assert";
+import React, { useState } from "react";
 
 import Logo from "../../img/Logo-con-colores.svg";
 
 import { StyledMainWrapper, StyledP } from "./styles";
 
-const LoginPage = ({
-    loginButtonCLicked,
-    inputOnChange,
-    inputAccountValue,
-    inputPasswordValue
-}: any) => {
+const LoginPage = (props: any) => {
+    const [inputAccountValue, setInputAccountValue] = useState({ value: "" });
+
+    const [inputPasswordValue, setInputPasswordValue] = useState({ value: "" });
+
+    const loginButtonCLicked = () => {
+        console.log("clicked!!!");
+    };
+
+    const AccountInputHandler = (event: any) => {
+        const newValue = event.target.value;
+        console.log(newValue);
+
+        return setInputAccountValue({ value: newValue });
+    };
+
+    const PasswordInputHandler = (event: any) => {
+        const newValue = event.target.value;
+        console.log(newValue);
+
+        return setInputPasswordValue({ value: newValue });
+    };
+
     return (
         <StyledMainWrapper>
             <div>
@@ -18,13 +36,13 @@ const LoginPage = ({
             </div>
             <form onSubmit={(event) => event.preventDefault}>
                 <input
-                    value={inputAccountValue}
-                    onChange={(event) => inputOnChange(event)}
-                    placeholder="Cuenta"></input>
+                    value={inputAccountValue.value}
+                    onChange={(event) => AccountInputHandler(event)}
+                    placeholder="Ingresa Tu Cuenta"></input>
                 <input
-                    value={inputPasswordValue}
-                    onChange={(event) => inputOnChange(event)}
-                    placeholder="Password"></input>
+                    value={inputPasswordValue.value}
+                    onChange={(event) => PasswordInputHandler(event)}
+                    placeholder="Contraseña"></input>
             </form>
             <button onClick={() => loginButtonCLicked()}>Ingresar</button>
         </StyledMainWrapper>

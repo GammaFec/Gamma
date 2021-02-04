b33456d1fdf73f9fc499470ceb9cb17ef3b46715
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-04 09:46:45 -0300

    Se agrego una P styled

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
+import { fontSizes, fonts } from "../../common/styles/index";
+
 const StyledMainWrapper = styled.main`
+    font-family: ${fonts.roboto}
     margin: auto;
     padding: 4rem 1rem 0px;
     text-align: center;
 `;
 
-export { StyledMainWrapper };
+const StyledP = styled.p`
+    font-size: ${fontSizes.font16};
+    font-weight: bold;
+    padding: 5px;
+    width: 80%;
+    margin: 1.5rem auto;
+`;
+
+export { StyledMainWrapper, StyledP };

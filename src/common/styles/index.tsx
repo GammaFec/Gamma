3190ffe6d9ac0ff61258aadb19ae945939d33b42
Anahi G <anahigaetan@hotmail.com>
2021-02-04 22:12:15 -0300

    Se agregaron font sizes requeridos para el diseño

--- src/common/styles/index.tsx
+++ src/common/styles/index.tsx
@@ -18,7 +18,10 @@ export const fontSizes = {
     font10: "10px",
     font12: "12px",
     font14: "14px",
-    font16: "16px"
+    font16: "16px",
+    font18: "18px",
+    font20: "20px",
+    font21: "21px"
 };
 
 //theme

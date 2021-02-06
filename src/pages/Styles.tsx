e83408540050af23d14ce058eaeecbd52af70771
jhonatanquirama <quiramapino47@gmail.com>
2021-02-05 22:58:54 -0500

    cambiando imagen y estilos

--- src/pages/Styles.tsx
+++ src/pages/Styles.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.div`
     width: 100%;
-    background-color: #ff8000;
     min-height: 100vh;
     display: flex;
     align-items: center;

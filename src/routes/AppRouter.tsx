40f5df9ea292075ba7c33cb3c20337aad45322c9
Nacho <nachoherrero93@hotmail.com>
2021-02-09 19:12:43 -0300

    Modifico la forma de importar el componente HomeScreen

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,9 +2,9 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "../components/Layout";
 import PrivateRoute from "./PrivateRoute";
-import index from "../pages/HomeScreen";
 
 const Login = React.lazy(() => import("../pages/Login"));
+const index = React.lazy(() => import("../pages/HomeScreen"));
 
 const AppRouter = () => {
     return (

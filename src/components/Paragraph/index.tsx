18a6190ba4dd58e9254c9bfdf382fb857a54aa47
Nacho <nachoherrero93@hotmail.com>
2021-02-09 18:42:07 -0300

    Elimino los componentes Paragraph y Title

--- src/components/Paragraph/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactElement } from "react";
-
-const Paragraph = ({ text }: { text: string }): ReactElement => <p>{text}</p>;
-
-export default Paragraph;
--- src/components/Title/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactElement } from "react";
-
-const Title = ({ title }: { title: string }): ReactElement => <h1>{title}</h1>;
-
-export default Title;

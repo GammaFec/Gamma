9832ea882c6899dba6d47a891122710c892376a2
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-10 09:57:30 -0300

    Se tuvo que modificar para que el obj Data funcione con el Src que react da a las imagenes

---
+++ src/components/Carousel/CarouselData.tsx
@@ -0,0 +1,31 @@
+import CarouselImg01 from "../../img/CarouselImage01.svg";
+import CarouselImg02 from "../../img/CarouselImage02.svg";
+import CarouselImg03 from "../../img/CarouselImage03.svg";
+
+const data = {
+    CarouselData: [
+        {
+            id: 1,
+            imageUrl: CarouselImg01,
+            imageAlt: "First Image",
+            text:
+                "Sabias que? Casi el 65% de Perros y gatos sufren en las calles de sudamerica. \n Es hora de cambiar eso y darles un hogar!! "
+        },
+        {
+            id: 2,
+            imageUrl: CarouselImg02,
+            imageAlt: "Second Image",
+            text:
+                "En esta aplicacion, podras descubir y dar en adopcion a increibles animales listos para hacerte felix y acompanarte en tu hogar."
+        },
+        {
+            id: 3,
+            imageUrl: CarouselImg03,
+            imageAlt: "Third Image",
+            text:
+                "Podras conversar con albergues y ademas de ello conocer perritos y gatitos listos para alegrarte el dia.\nRecuerda que ellos son una extension de nosotros y no hay algo mas fiel que ellos. "
+        }
+    ]
+};
+
+export default data;

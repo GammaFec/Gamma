3f8772d2372224c21b1936142ce299cc772342f6
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-10 11:00:16 -0300

    Se agregaron los estilos correspondientes tomados de figma

--- src/components/Carousel/styles.tsx
+++ src/components/Carousel/styles.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
+import { fonts } from "../../common/styles/index";
 
 const StyledLi = styled.li<{ active: boolean }>`
-    z-index: ${(props) => (props.active ? "10" : "-10")};
+    z-index: ${(props) => (props.active ? "0" : "-10")};
     opacity: ${(props) => (props.active ? 1 : 0)};
     transition: 0.7s cubic-bezier(0, 0.84, 0.81, 1.01);
     overflow: hidden;
 
     list-style: none;
     width: ${(props) => (props.active ? "100%" : "0")};
-    height: 30rem;
     top: 0;
 
     & > img {
@@ -22,12 +22,18 @@ const StyledLi = styled.li<{ active: boolean }>`
         margin: 2rem 2rem;
     }
 `;
-//
-//
 
 const StyledUl = styled.ul`
     display: flex;
-    padding: 0;
+    padding: 0 10px;
+    height: 30rem;
+    max-height: 450px;
+    font-family: ${fonts.roboto};
+
+    @media (min-width: 400px) {
+        max-height: none;
+        height: 470px;
+    } ;
 `;
 
 export { StyledLi, StyledUl };

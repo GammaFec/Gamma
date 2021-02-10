a1e9a32fcda9a3d422bf62d1d35b5dbe432474fc
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-10 20:16:39 -0300

    Se modifico Segun comentarios Se cambiarion todos las medidas de rem por px

--- src/components/Carousel/styles.tsx
+++ src/components/Carousel/styles.tsx
@@ -3,35 +3,34 @@ import { fonts } from "../../common/styles/index";
 
 const StyledLi = styled.li<{ active: boolean }>`
     z-index: ${(props) => (props.active ? "0" : "-10")};
-    opacity: ${(props) => (props.active ? 1 : 0)};
-    transition: 0.7s cubic-bezier(0, 0.84, 0.81, 1.01);
+    opacity: ${(props) => (props.active ? "1" : "0")};
+    transition: 0.5s cubic-bezier(0, 0.84, 0.81, 1.01);
     overflow: hidden;
+    position: ${(props) => (props.active ? "relative" : "absolute")};
+    top: ${(props) => (props.active ? "0" : "-30px")};
 
     list-style: none;
-    width: ${(props) => (props.active ? "100%" : "0")};
-    top: 0;
+    width: 100%;
 
     & > img {
         width: 70%;
         max-width: 300px;
         height: auto;
-        padding: 1rem;
+        padding: 16px;
     }
 
     & > p {
-        margin: 2rem 2rem;
+        margin: 25px;
     }
 `;
 
 const StyledUl = styled.ul`
     display: flex;
     padding: 0 10px;
-    height: 30rem;
-    max-height: 450px;
+    height: 450px;
     font-family: ${fonts.roboto};
 
     @media (min-width: 400px) {
-        max-height: none;
         height: 470px;
     } ;
 `;

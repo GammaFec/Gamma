ee3b3e8cd92eab2c38806fc7f03167f6a1baacc6
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-02-12 21:27:29 -0300

    actualizo los componentes del router

---
+++ src/routes/AppRouter/index.tsx
@@ -0,0 +1,30 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import RouteItem from "../RouteItem";
+import ROUTES from "../routes";
+
+import Splash from "../../pages/Splash";
+
+const AppRouter = (): JSX.Element => {
+    return (
+        <>
+            <Suspense fallback={Splash}>
+                <Router>
+                    <Switch>
+                        {ROUTES.map((route) => (
+                            <RouteItem
+                                component={route.component}
+                                exact={route.exact && route.exact}
+                                key={route.name}
+                                path={route.path}
+                            />
+                        ))}
+                    </Switch>
+                </Router>
+            </Suspense>
+        </>
+    );
+};
+
+export default AppRouter;
---
+++ src/routes/RouteItem/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { PATHS } from "../paths";
+import IRoute from "../types";
+
+function RouteItem({ component: Component, ...rest }: IRoute): JSX.Element {
+    const someLoginValidation = () => true;
+
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                someLoginValidation() ? <Component {...rest} /> : <Redirect to={PATHS.Login} />
+            }
+        />
+    );
+}
+
+export default RouteItem;

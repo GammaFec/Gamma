39b930794273f93125f34cf73611049594748ad5
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-02-12 21:27:13 -0300

    actualizo el archivo de rutas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,20 +1,33 @@
 import React from "react";
+import { PATHS } from "./paths";
+import Route from "./types";
 
 const Splash = React.lazy(() => import("../pages/Splash"));
 const NotFound = React.lazy(() => import("../pages/NotFound"));
+const Login = React.lazy(() => import("../pages/Login"));
+const HomeScreen = React.lazy(() => import("../pages/HomeScreen"));
 
-const ROUTES = [
-    // {
-    //     path: "/",
-    //     component: Home,
-    //     name: "Home",
-    //     exact: true
-    // },
+const ROUTES: Route[] = [
     {
-        path: "/Splash",
+        path: PATHS.Splash,
         component: Splash,
         name: "Splash",
-        exact: true
+        exact: true,
+        isPrivate: false
+    },
+    {
+        path: PATHS.Login,
+        component: Login,
+        name: "Login",
+        exact: true,
+        isPrivate: false
+    },
+    {
+        path: PATHS.HomeScreen,
+        component: HomeScreen,
+        name: "HomeScreen",
+        exact: true,
+        isPrivate: false
     },
     {
         component: NotFound,

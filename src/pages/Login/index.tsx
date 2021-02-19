daa9cf9096553f55b00dde226da711c86ddc1998
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-02-19 11:39:12 -0300

    Cambio en la importacion de la funcion InputIsValid

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
-import { InputIsValid } from "./utils";
+import { InputIsValid } from "../../utils/InputValidation";
 import Logo from "../../img/Logo.svg";
 import * as variantType from "../../common/constants";
 import { StyledMainWrapper, StyledP } from "./styles";

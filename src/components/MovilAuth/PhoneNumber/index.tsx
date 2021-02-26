e40ce771bc1ed5143373891ac672d32d2ae949a4
Your Name <nachoherrero93@hotmail.com>
2021-02-26 13:26:03 -0300

    agrego estilos a PhoneNumber

--- src/components/MovilAuth/PhoneNumber/index.tsx
+++ src/components/MovilAuth/PhoneNumber/index.tsx
@@ -1,22 +1,32 @@
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/material.css";
-
 import React from "react";
 import iPhoneNumber from "./types";
+import { colors, fonts, fontSizes } from "../../../common/styles";
 
 const PhoneNumber: React.FC<iPhoneNumber> = ({ handleChange, value }: iPhoneNumber) => {
     const change = (value: string) => handleChange?.(value);
-
     return (
         <div>
             <PhoneInput
+                buttonStyle={{ paddingLeft: "53px" }}
                 country={"us"}
-                value={value}
+                enableLongNumbers={true}
+                inputStyle={{
+                    backgroundColor: `${colors.lightGray}`,
+                    color: `${colors.gray}`,
+                    fontSize: `${fontSizes.font12}`,
+                    fontFamily: `${fonts.poppins}`,
+                    border: "none",
+                    paddingLeft: "92px",
+                    width: "254px",
+                    height: "42px"
+                }}
+                masks={{ ar: "... ...-...." }}
                 onChange={change}
                 preferredCountries={["ar"]}
                 prefix="+"
-                enableLongNumbers={true}
-                masks={{ ar: "... ...-...." }}
+                value={value}
             />
         </div>
     );

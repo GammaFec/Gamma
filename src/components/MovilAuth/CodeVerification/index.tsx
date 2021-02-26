3537016166ba9ad97e28f67be5d5fa3875b88b75
Your Name <nachoherrero93@hotmail.com>
2021-02-26 13:19:54 -0300

    agrego estilos a CodeVerification

--- src/components/MovilAuth/CodeVerification/index.tsx
+++ src/components/MovilAuth/CodeVerification/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactCodeInput from "react-verification-code-input";
 import iCodeVerification from "./types";
+import "./styles.css";
 
 const CodeVerification: React.FC<iCodeVerification> = ({
     handleComplete,
     handleChange
 }: iCodeVerification): JSX.Element => {
-    return <ReactCodeInput onComplete={handleComplete} onChange={handleChange} />;
+    return (
+        <ReactCodeInput className="estilos" onChange={handleChange} onComplete={handleComplete} />
+    );
 };
 
 export default CodeVerification;

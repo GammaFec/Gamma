17eb49bf856742257e2f7f51ae388707b59987bd
Your Name <nachoherrero93@hotmail.com>
2021-02-26 13:24:46 -0300

    agrego estilos al componente Form

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import { fonts } from "../../common/styles";
 import iForm from "./types";
 
 export const StyledForm = styled.form<iForm>`
+    font-family: ${fonts.poppins};
     text-align: ${({ isContentCentered }) => (isContentCentered ? "center" : "left")};
+    margin-top: 57px;
 `;

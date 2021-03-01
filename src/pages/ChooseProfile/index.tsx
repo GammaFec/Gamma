86f1fdf80837d2cbca2ece7bf27964c53d503f34
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-02-28 22:21:53 -0300

    styles to title and text

--- src/pages/ChooseProfile/index.tsx
+++ src/pages/ChooseProfile/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import ProfileRoleContainer from "./ProfileRoleContainer";
-import { StyledContainer, StyledMain, StyledText } from "./styles";
+import { StyledContainer, StyledMain, StyledText, StyledTitle } from "./styles";
 
 const ChooseProfile: React.FC = (): JSX.Element => {
     return (
         <StyledMain>
             <StyledContainer>
-                <h1>Elige tu perfil deseado</h1>
+                <StyledTitle>Elige tu perfil deseado</StyledTitle>
                 <StyledText>
                     En PetMatch nos importas mucho, por eso selecciona que papel deseas tener en la
                     aplicación para brindarte una experiencia única.
--- src/pages/ChooseProfile/styles.tsx
+++ src/pages/ChooseProfile/styles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { fontSizes } from "../../common/styles";
 
 export const StyledMain = styled.main`
     display: flex;
@@ -8,12 +9,18 @@ export const StyledMain = styled.main`
     width: 100vw;
 `;
 
-export const StyledText = styled.p`
-    line-height: 150%;
-`;
-
 export const StyledContainer = styled.div`
     text-align: center;
     padding: 1rem;
     max-width: 90%;
+`;
+
+export const StyledTitle = styled.h1`
+    font-size: ${fontSizes.font21};
+    line-height: 31.5px;
+`;
+
+export const StyledText = styled.p`
+    line-height: 18px;
+    font-size: ${fontSizes.font12};
 `;

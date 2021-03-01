0a2e2582b8f4f03e9e39982e8331424c8fbadc6b
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-01 12:43:01 -0300

    Se agrego la data que corresponde a un shelter

--- src/components/Carousel/CarouselData.tsx
+++ src/components/Carousel/CarouselData.tsx
@@ -2,8 +2,35 @@ import CarouselImg01 from "../../img/CarouselImage01.svg";
 import CarouselImg02 from "../../img/CarouselImage02.svg";
 import CarouselImg03 from "../../img/CarouselImage03.svg";
 
+// The images that correspond to a Shelter User are still to be determinated
+
 const data = {
-    CarouselData: [
+    adopterData: [
+        {
+            id: 1,
+            imageUrl: CarouselImg01,
+            imageAlt: "First Image",
+            titleOnPage: "Elige tu compañero ideal",
+            text:
+                "Aquí el amor a primera vista si existe, descubre tu compañero perfecto y vive muchas historias junto a ellos."
+        },
+        {
+            id: 2,
+            imageUrl: CarouselImg02,
+            imageAlt: "Second Image",
+            titleOnPage: "Conversa con Albergues",
+            text:
+                "Heroes sin capa día a día brindan atención, haz match con alguna de las mascotas listas en adopción y define el proceso de adopción."
+        },
+        {
+            id: 3,
+            imageUrl: CarouselImg03,
+            imageAlt: "Third Image",
+            titleOnPage: "Recuerda siempre",
+            text: "Cuida, protege y dale amor a quien con solo verte se emociona sin límites."
+        }
+    ],
+    shelterData: [
         {
             id: 1,
             imageUrl: CarouselImg01,

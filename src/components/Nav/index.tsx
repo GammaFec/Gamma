bd07902468f34b3644b95069630dfcf95bae230b
Cyn Romero <cyn.jaz.romero@gmail.com>
2021-03-10 20:52:28 -0300

    Quito estado, event handler y componente Menú del Nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 //styled components
 import { StyledNav, StyledIcon } from "./styles";
@@ -6,18 +6,11 @@ import { StyledNav, StyledIcon } from "./styles";
 import Logo from "../../img/Logo.svg";
 import MenuBtn from "../../img/gg_menu-left.png";
 import MsgBtn from "../../img/bx-message.png";
-//components
-import Menu from "../Menu/index";
 
 const Nav: React.FC = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-
-    const handleClick = (): void => setIsOpen(!isOpen);
-
     return (
         <StyledNav>
-            <StyledIcon alt="Menu button" onClick={handleClick} src={MenuBtn} />
-            <Menu handleClick={handleClick} isOpen={isOpen} />
+            <StyledIcon alt="Menu button" src={MenuBtn} />
             <Link to="/">
                 <StyledIcon alt="Logo" src={Logo} />
             </Link>

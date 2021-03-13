e5ff3bef32719b217fbfa19fb42d6c8a2bd7fbdf
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-13 10:20:16 -0300

    Se agrego la funcion Click con un console log para que pueda correr sin problemas

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -8,9 +8,11 @@ const CarouselPage: React.FC = () => {
     const isAdopter = true; // useContext() To be determined, the logic was build using booleans
     const carrouselData = isAdopter ? data.adopterData : data.shelterData;
 
+    const click = (): void => console.log("clicked!!");
+
     return (
         <StyledMain>
-            <Carrousel carouselData={carrouselData} />
+            <Carrousel carouselData={carrouselData} click={click} />
         </StyledMain>
     );
 };

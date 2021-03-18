d6d71283f2a76d0f1e40c737ebda8f0e53d5faae
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-18 11:01:34 -0300

    cambios requeridos en el CSS

--- src/components/Filter/FilterTab/styles.tsx
+++ src/components/Filter/FilterTab/styles.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { theme, fontSizes } from "../../../common/styles/index";
+
 const StyledImg = styled.img`
     margin: 0 auto;
 `;
@@ -7,12 +9,22 @@ const StyledImg = styled.img`
 const StyledDiv = styled.div`
     height: 63px;
     width: 63px;
-    margin: 0 10px;
+    margin: 0 10px 6px 10px;
+    background-color: ${theme.colorPrimary};
+    border-radius: 7px;
+    text-align: center;
 `;
 
 const StyledWrapper = styled.div`
     display: flex;
-    margin: 0 auto;
+    flex-flow: column;
+
+    p {
+        letter-spacing: -0.44px;
+        font-size: ${fontSizes.font12};
+        margin: 0 auto;
+        font-family: ${theme.fontPrimary};
+    }
 `;
 
 export { StyledImg, StyledDiv, StyledWrapper };

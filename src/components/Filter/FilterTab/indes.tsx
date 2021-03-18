459f274033d0b1f08eaf1a04cbdacc1d58f56b49
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-18 09:15:29 -0300

    Estructura y estilos basicos de la Tab para cada filtro

---
+++ src/components/Filter/FilterTab/indes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyledImg, StyledDiv, StyledWrapper } from "./styles";
+
+const FilterTab: React.FC = () => {
+    return (
+        <StyledWrapper>
+            <StyledDiv>
+                <StyledImg alt="Hola" src="local/" />
+            </StyledDiv>
+            <p></p>
+        </StyledWrapper>
+    );
+};
+
+export default FilterTab;
---
+++ src/components/Filter/FilterTab/styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledImg = styled.img`
+    margin: 0 auto;
+`;
+
+const StyledDiv = styled.div`
+    height: 63px;
+    width:63px;
+    margin 0 10px;
+`;
+
+const StyledWrapper = styled.div`
+    display: flex;
+    margin: 0 auto;
+`;
+
+export { StyledImg, StyledDiv, StyledWrapper };

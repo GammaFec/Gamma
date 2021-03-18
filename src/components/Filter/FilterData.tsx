74bd4cedf7fb034066741b53d0cee6e1591c2ce6
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-18 09:29:45 -0300

    estructura preeliminar del la data que requiere el componente

---
+++ src/components/Filter/FilterData.tsx
@@ -0,0 +1,29 @@
+import FilterTabDog from "../../assets/img/FilterTabDog.svg";
+import FilterTabAdultDog from "../../assets/img/FilterTabAdultDog.svg";
+import FilterTabCat from "../../assets/img/FilterTabCat.svg";
+import FilterTabPuppyDog from "../../assets/img/FilterTabPuppyDog.svg";
+
+const Data = [
+    {
+        filterName: "Perros",
+        filterImageSrc: FilterTabDog,
+        filterImageAlt: "Un perro sentado"
+    },
+    {
+        filterName: "Gatos",
+        filterImageSrc: FilterTabCat,
+        filterImageAlt: "Un gato sentado"
+    },
+    {
+        filterName: "Cachorros",
+        filterImageSrc: FilterTabPuppyDog,
+        filterImageAlt: "Un perro cachorro de pie"
+    },
+    {
+        filterName: "Adultos",
+        filterImageSrc: FilterTabAdultDog,
+        filterImageAlt: "Un perro adulto de pie"
+    }
+];
+
+export default Data;

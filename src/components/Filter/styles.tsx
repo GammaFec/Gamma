4771be58272010d10fcfa82eadd4b1016db8e528
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-18 14:24:43 -0300

    Se redujo un margin innecesario

--- src/components/Filter/styles.tsx
+++ src/components/Filter/styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 const StyledWrapper = styled.div`
     display: flex;
     padding: 0 14px;
-    margin: 50px auto;
+    margin: 10px auto;
 `;
 
 export { StyledWrapper };

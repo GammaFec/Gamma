8f09e960864ee6cb0ba5cb94424eaef202cf551a
Cyn Romero <cyn.jaz.romero@gmail.com>
2021-03-23 16:23:51 -0300

    Quito el componente menú de la pág NotFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Menu from "../components/Menu/index";
 
 const NotFound: React.FC = () => {
-    return (
-        <>
-            <Menu isOpen userName="Sebastian Gonzales" userType="Adoptante" />
-            <div>Not Found</div>
-        </>
-    );
+    return <div>Not Found</div>;
 };
 
 export default NotFound;

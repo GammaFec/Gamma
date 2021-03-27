15c411648b7b4f959805424695d3d3823d3c3b3a
Your Name <nachoherrero93@hotmail.com>
2021-03-27 15:15:38 -0300

    Agrego tipado

---
+++ src/components/ContainerPet/CardPet/types.tsx
@@ -0,0 +1,11 @@
+export interface ICardPet {
+    name: string;
+    imageUrl: string;
+    alt?: string;
+    breed: string;
+    age: string;
+    type: string;
+    genre: string;
+    distance: string;
+    dateOfBirth: string;
+}

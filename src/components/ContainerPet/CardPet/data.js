f58cac966503eec72bf717e2f8cca83a2c9d59c0
Your Name <nachoherrero93@hotmail.com>
2021-03-27 15:16:31 -0300

    datos de prueba

---
+++ src/components/ContainerPet/CardPet/data.js
@@ -0,0 +1,40 @@
+import Rocky from "../../../assets/img/Rocky.svg";
+import Chikorita from "../../../assets/img/Chikorita.svg";
+import Sasha from "../../../assets/img/Sasha.svg";
+
+export const animal = [
+    {
+        name: "Roky",
+        imageUrl: Rocky,
+        alt: "",
+        breed: "Dalmata",
+        age: "3",
+        type: "dog",
+        genre: "male",
+        distance: "3,4 km",
+        dateOfBirth: "20180326"
+    },
+
+    {
+        name: "Sasha",
+        imageUrl: Sasha,
+        alt: "",
+        breed: "Criollo",
+        age: "1",
+        type: "dog",
+        genre: "famale",
+        distance: "1,2 km",
+        dateOfBirth: "20200105"
+    },
+    {
+        name: "Chikorita",
+        imageUrl: Chikorita,
+        alt: "",
+        breed: "Schnauzer",
+        age: "1",
+        type: "dog",
+        genre: "famale",
+        distance: "2,2 km",
+        dateOfBirth: "20210210"
+    }
+];

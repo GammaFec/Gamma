ba7eaa4692c26d3c3ed7ca8f895bc64b6f9d9397
Your Name <nachoherrero93@hotmail.com>
2021-03-29 18:04:13 -0300

    agrego i18n a ContainerCard

--- src/components/ContainerPet/index.tsx
+++ src/components/ContainerPet/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { nanoid as id } from "nanoid";
+import { useTranslation } from "react-i18next";
 
 // data
 import { animal } from "./CardPet/data";
@@ -11,9 +12,11 @@ import CardPet from "./CardPet";
 import { StyledContainer } from "./styles";
 
 const ContainerCard = (): JSX.Element => {
+    const { t } = useTranslation();
+
     return (
         <StyledContainer>
-            <h1>Cerca de ti</h1>
+            <h1>{t("ContainerCard:h1")}</h1>
             {animal.map(({ genre, breed, age, type, name, imageUrl, distance, dateOfBirth }) => (
                 <CardPet
                     age={age}

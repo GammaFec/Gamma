e7bc333dd9ba9ad8ea3a2696d91547de54cb0a76
Your Name <nachoherrero93@hotmail.com>
2021-03-29 18:10:28 -0300

    refactorizo la función de calcular la edad

--- src/utils/ageCalculate.tsx
+++ src/utils/ageCalculate.tsx
@@ -6,34 +6,14 @@ export const ageCalculate = (dateOfBirth: string): string => {
     const monthsOfLife = moment().diff(dateOfBirth, "months");
     let resultAnimalAge;
 
-    switch (monthsOfLife) {
-        case 1:
+    switch (true) {
+        case monthsOfLife === 1:
             resultAnimalAge = `1 ${t("CardPet:month")}`;
             break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-        case 11:
+        case monthsOfLife >= 2 && monthsOfLife <= 11:
             resultAnimalAge = `${moment().diff(dateOfBirth, "months")}  ${t("CardPet:month")}s`;
             break;
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-        case 16:
-        case 17:
-        case 18:
-        case 19:
-        case 20:
-        case 21:
-        case 22:
-        case 23:
+        case monthsOfLife >= 12 && monthsOfLife <= 23:
             resultAnimalAge = `1 ${t("CardPet:year")}`;
             break;
         default:

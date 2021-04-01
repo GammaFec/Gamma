9f7e8e308b0b8aefe78b292f81be8340b7be3c2b
Your Name <nachoherrero93@hotmail.com>
2021-04-01 11:00:11 -0300

    modifico archivo i18n.ts

--- src/components/ContainerPet/i18n.ts
+++ src/components/ContainerPet/i18n.ts
@@ -2,9 +2,9 @@ import { LanguageList } from "./../../i18next/config";
 import i18next from "i18next";
 
 i18next.addResources(LanguageList.SPANISH, "ContainerCard", {
-    h1: "Cerca de ti"
+    title: "Cerca de ti"
 });
 
 i18next.addResources(LanguageList.ENGLISH, "ContainerCard", {
-    h1: "Around here"
+    title: "Around here"
 });

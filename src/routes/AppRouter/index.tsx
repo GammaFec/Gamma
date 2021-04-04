0adf3948fb8fe8b3dddd93cee8b8235907a54469
Anahi G <anahigaetan@hotmail.com>
2021-04-04 19:58:43 -0300

    Splash se muestra al iniciar la app y luego de 3 segundos redirige a la pantalla de pre login

--- src/routes/AppRouter/index.tsx
+++ src/routes/AppRouter/index.tsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 import RouteItem from "../RouteItem";
 import ROUTES from "../routes";
-
+import { PATHS } from "../paths";
 import Splash from "../../pages/Splash";
 
 const AppRouter = (): JSX.Element => {
@@ -12,6 +12,9 @@ const AppRouter = (): JSX.Element => {
             <Suspense fallback={Splash}>
                 <Router>
                     <Switch>
+                        <Route exact path="/">
+                            <Redirect to={PATHS.Splash} />
+                        </Route>
                         {ROUTES.map((route) => (
                             <RouteItem
                                 component={route.component}

153ffee012596caa1adea3c08fd2daf0e7c826c2
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-04-07 16:20:02 -0400

    Arreglar algunos errores de compilacion

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Carousel from "../components/Carousel";
+import data from "../components/Carousel/CarouselData";
 
 const NotFound: React.FC = () => {
-    return <div>NotFound</div>;
+    const { adopterData } = data;
+    // eslint-disable-next-line no-console
+    console.log(adopterData);
+    return <Carousel carouselData={adopterData} click={(): void => alert("click")} />;
 };
 
 export default NotFound;

177b59a7671c8442a0c944b84e968a841b6dfe7c
Your Name <nachoherrero93@hotmail.com>
2021-04-23 12:01:55 -0300

    agrego en constants un archivo con los svgs

---
+++ src/common/constants/svgs.tsx
@@ -0,0 +1,7 @@
+import { ReactComponent as HandsHone } from "../../assets/img/hands-hone.svg";
+import { ReactComponent as HandsOk } from "../../assets/img/hands-phone-ok.svg";
+import { ReactComponent as Pen } from "../../assets/img/pen.svg";
+
+export const HANDSHONE_SVG = HandsHone;
+export const HANDSOK_SVG = HandsOk;
+export const PEN_SVG = Pen;

9d170624d02ffaeb033e7feac29aa7ee5fe96930
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-05-11 14:14:45 -0400

    Testing Nav

---
+++ src/components/Nav/Nav.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Nav from "./";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
+import { PATHS } from "../../routes/paths";
+
+jest.mock("react-i18next", () => ({
+    useTranslation: (): { t: (key: string) => string } => ({ t: (key: string): string => key })
+}));
+
+describe("<Nav />", () => {
+    const history = createMemoryHistory();
+
+    beforeEach(() => {
+        history.push(PATHS.Home);
+    });
+
+    test("Render", () => {
+        const { getByAltText } = render(<Nav />);
+        getByAltText("Nav:MenuAlt");
+    });
+
+    test("Handle go to splash", () => {
+        const { getByAltText } = render(
+            <Router history={history}>
+                <Nav />
+            </Router>
+        );
+
+        const Button = getByAltText("Nav:LogoAlt");
+
+        fireEvent.click(Button);
+
+        expect(history.location.pathname).toEqual(PATHS.Splash);
+    });
+
+    test("Handle go to messages", () => {
+        const { getByAltText } = render(
+            <Router history={history}>
+                <Nav />
+            </Router>
+        );
+
+        const Button = getByAltText("Nav:MessagesAlt");
+
+        fireEvent.click(Button);
+
+        expect(history.location.pathname).toEqual(PATHS.Messages);
+    });
+});

80956a854565e3cc4b0d30cd4f9ea511baff4885
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-05-14 13:40:38 -0400

    Arreglo el test de <ButtonBack />

--- src/components/ButtonBack/ButtonBack.test.tsx
+++ src/components/ButtonBack/ButtonBack.test.tsx
@@ -4,10 +4,8 @@ import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 
 describe("<ButtonBack />", () => {
-    const ButtonContent = "Atras";
-
     test("render content", () => {
         const { getByText } = render(<ButtonBack />);
-        getByText(ButtonContent);
+        expect(getByText("Atras")).toBeInTheDocument();
     });
 });

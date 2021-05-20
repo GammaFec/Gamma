1883b9e80ad7c0440adf017d2b8c8922e68f23ce
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-05-20 17:17:35 -0400

    Testing splash

---
+++ src/pages/Splash/Splash.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Splash from "./";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "jest-styled-components";
+
+jest.mock("react-i18next", () => ({
+    useTranslation: (): { t: (key: string) => string } => ({ t: (key: string): string => key })
+}));
+
+describe("<Splash />", () => {
+    test("Render", () => {
+        const { asFragment } = render(<Splash />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test("Render all childrens", () => {
+        const { getByText, getByAltText } = render(<Splash />);
+
+        getByAltText("ImageAlt");
+        getByText("Adogtemos");
+    });
+});

5d953712d7bd3efb0e236ee96f14af9e8d5ff4d1
hadita <anahigaetan@hotmail.com>
2021-07-24 19:01:38 -0300

    Arreglo ubicacion de archivos en carpeta common y actualizo los imports

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,7 @@ import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { InputIsValid } from "../../utils/InputValidation";
 import Logo from "../../assets/img/Logo.svg";
-import * as variantType from "../../common/styles/constants";
+import * as variantType from "../../common/constants";
 import { StyledMainWrapper, StyledP, StyledForm } from "./styles";
 import eye from "../../assets/img/eye.svg";
 import { useTranslation } from "react-i18next";

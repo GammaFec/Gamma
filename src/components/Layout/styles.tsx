9c1b0ded85d89aeaa068f29896fac8a2c591290f
hadita <anahigaetan@hotmail.com>
2021-07-31 22:06:02 -0300

    Agrego ultimos ajustes en los estilos del componente Layout

--- src/components/Layout/styles.tsx
+++ src/components/Layout/styles.tsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const StyledLayout = styled.div`
-    width: 100%;
+    max-width: 425px;
     min-height: 100vh;
+    margin: auto;
+    padding: 25px;
     display: flex;
     flex-direction: column;
-    background-color: lightblue;
 `;
+
 export const StyledLayoutChildren = styled.div`
-    width: 100%;
-    margin: 0 auto;
     flex-grow: 1;
-    padding: 0 10px;
-    background-color: lightgreen;
+    display: flex;
+    flex-direction: column;
 `;

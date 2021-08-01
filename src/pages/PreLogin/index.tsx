1143ac583e3eaad5421a93fc0e62ec32c7ea4b38
hadita <anahigaetan@hotmail.com>
2021-07-31 22:06:32 -0300

    Agrego el Layout en el componente Pre-Login

--- src/pages/PreLogin/index.tsx
+++ src/pages/PreLogin/index.tsx
@@ -4,11 +4,12 @@ import React, { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 
 // Styled-components
-import { StyledHome, StyledHeader, StyledMain, StyledImg, StyledH1 } from "./styles";
+import { StyledHome, StyledHeader, StyledMain, StyledImg, StyledH1, buttonStyles } from "./styles";
 
 // Components
 import Logo from "../../assets/img/Logo.svg";
 import Button from "../../components/Button";
+import Layout from "../../components/Layout";
 import { PRIMARY, SECONDARY } from "../../common/styles/constants";
 import { useHistory } from "react-router-dom";
 import { PATHS } from "../../routes/paths";
@@ -26,20 +27,22 @@ const PreLogin = (): ReactElement => {
     };
 
     return (
-        <StyledHome>
-            <StyledHeader>
-                <StyledImg alt={t("PreLogin:ImageAlt")} src={Logo} />
-                <StyledH1>{t("PreLogin:Title")}</StyledH1>
-            </StyledHeader>
-            <StyledMain>
-                <Button handleClick={handleLogIn} variant={PRIMARY}>
-                    {t("PreLogin:LogIn")}
-                </Button>
-                <Button handleClick={handleRegister} variant={SECONDARY}>
-                    {t("PreLogin:SignUp")}
-                </Button>
-            </StyledMain>
-        </StyledHome>
+        <Layout>
+            <StyledHome>
+                <StyledHeader>
+                    <StyledImg alt={t("PreLogin:ImageAlt")} src={Logo} />
+                    <StyledH1>{t("PreLogin:Title")}</StyledH1>
+                </StyledHeader>
+                <StyledMain>
+                    <Button handleClick={handleLogIn} styles={buttonStyles} variant={PRIMARY}>
+                        {t("PreLogin:LogIn")}
+                    </Button>
+                    <Button handleClick={handleRegister} styles={buttonStyles} variant={SECONDARY}>
+                        {t("PreLogin:SignUp")}
+                    </Button>
+                </StyledMain>
+            </StyledHome>
+        </Layout>
     );
 };
 
